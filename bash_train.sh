45e503bcf40e42cdd83443a57d1198380ed2acaf
OscarVallsLab <oscarvalls.cvblab@gmail.com>
2024-05-24 13:16:31 +0200

    Fixed dependencies and NAS

--- bash_train.sh
+++ bash_train.sh
@@ -1,5 +1,7 @@
+export PYTHONPATH=$PYTHONPATH:/workspace/code
+
 # Train the AudioLDM (latent diffusion part)
-python3 audioldm_train/train/latent_diffusion.py -c audioldm_train/config/2023_08_23_reproduce_audioldm/audioldm_crossattn_flant5.yaml
+python3 audioldm_train/train/latent_diffusion.py -c audioldm_train/config/2023_08_23_reproduce_audioldm/audioldm_original.yaml
 
 # Train the VAE
 # python3 audioldm_train/train/autoencoder.py -c audioldm_train/config/2023_11_13_vae_autoencoder/16k_64.yaml
